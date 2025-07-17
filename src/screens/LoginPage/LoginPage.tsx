648b9a8f5a372e66070ea2e01c557a7432a0901f
Michał Górny <umpex94@gmail.com>
2025-07-17 18:35:41 +0200

    Making simple form and validation

---
+++ src/screens/LoginPage/LoginPage.tsx
@@ -0,0 +1,42 @@
+import LoginForm from "./LoginForm";
+
+const LoginPage = () => {
+  return (
+    <>
+      <nav
+        style={{
+          width: "100%",
+          textAlign: "center",
+          padding: "20px 0",
+          fontSize: "34px",
+        }}
+      >
+        <a href="#">😀 Mood tracker</a>
+      </nav>
+      <div
+        className="login-form-container"
+        style={{
+          margin: "auto",
+          display: "flex",
+          flexDirection: "column",
+          rowGap: "10px",
+        }}
+      >
+        <div className="login-form-header">
+          <h2 className="form-header">Welcome back!</h2>
+          <small className="form-header-description">
+            Log in to continue tracking your mood and sleep
+          </small>
+        </div>
+        <LoginForm />
+        <div className="login-form-footer">
+          <span className="form-footer">
+            Haven't got an account? <a href="">Sign up.</a>
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LoginPage;
